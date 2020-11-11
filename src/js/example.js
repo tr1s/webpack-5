d5f531c2e182d7eb674ded4fd77f67f6141f11c6
Tristan Lawrence <trislaw@gmail.com>
2020-11-11 08:30:44 -0500

    init commit

---
+++ src/js/example.js
@@ -0,0 +1,16 @@
+console.log(
+  '%c Hello from /scripts/script.js ',
+  'background: lemonchiffon; border: 1px solid #fff'
+);
+console.log(
+  '%c Welcome to tris-webpack-boilerplate! ',
+  'background: lemonchiffon; border: 1px solid #fff'
+);
+console.log(
+  '%c Coded by: https://tris.codes 💠',
+  'background: lavenderblush; border: 1px solid #000; padding: 4px; padding-top: 10px; padding-bottom: 8px;'
+);
+
+export const example = () => {
+  return 'Sensible webpack 5 boilerplate using Babel and PostCSS with a hot dev server and an optimized production build.'
+}
---
+++ webpack.common.js
@@ -0,0 +1,89 @@
+const paths = require('./paths')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [
+    paths.src + '/index.js'
+  ],
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    assetModuleFilename: 'assets/[name][ext]',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline'
+      },
+      {
+        test: /\.js$/, exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+        },
+      ],
+    }),
+    // Generates an HTML file from a template
+    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
+    new HtmlWebpackPlugin({
+      title: 'tris webpack boilerplate',
+      favicon: paths.src + '/images/favicon.png',
+      template: paths.src + '/template.html', // template file
+      filename: 'index.html', // output file
+    }),
+    new HtmlWebpackPlugin({
+      title: 'tris webpack boilerplate | 404',
+      favicon: paths.src + '/images/favicon.png',
+      template: paths.src + '/template-404.html', // template file
+      filename: '404.html', // output file
+    }),
+  ]
+}
